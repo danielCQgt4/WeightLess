20b35c10c7e989f43b9156032e6a4f8b82d59f36
Santiago Hurtado <santiagohurtado99@hotmail.com>
2020-10-24 21:38:09 -0600

    Feat: work in publications wip

--- FrontEnd/Controllers/PublicationController.cs
+++ FrontEnd/Controllers/PublicationController.cs
@@ -1,21 +1,32 @@
-﻿using System;
+﻿using Backend.DAL;
+using Backend.Entity;
+using FrontEnd.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace FrontEnd.Controllers
-{
-    public class PublicationController : Controller
-    {
+namespace FrontEnd.Controllers {
+    public class PublicationController : Controller {
         // GET: Publication
-        public ActionResult Index()
-        {
-            return View();
+        public ActionResult Index() {
+            List<Publication> publications;
+
+            using (var unidad = new UnitWork<Publication>()) {
+                publications = unidad.genericDAL.GetAll().ToList();
+            }
+
+            List<PublicationViewModel> publicationsVM = new List<PublicationViewModel>();
+
+            publicationsVM = PublicationViewModel.Converter(publications);
+
+
+
+            return View(publicationsVM);
         }
 
-        public ActionResult Create()
-        {
+        public ActionResult Create() {
             return View();
         }
     }
--- FrontEnd/Models/PublicationViewModel.cs
+++ FrontEnd/Models/PublicationViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Backend.Entity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -12,12 +13,19 @@ namespace FrontEnd.Models {
         public int idPublication { get; set; }
 
         [Required]
+        [DisplayName("Fecha")]
         public DateTime datetime { get; set; }
+
         [Required]
+        [DisplayName("Título")]
         public string title { get; set; }
+
         [Required]
+        [DisplayName("Descripción")]
         public string description { get; set; }
+
         [Required]
+        [DisplayName("Tipo")]
         public string type { get; set; }
 
         //TEMP
@@ -43,8 +51,8 @@ namespace FrontEnd.Models {
             };
         }
 
-        public static PublicationViewModel Converter(PublicationViewModel publication) {
-            return new PublicationViewModel() {
+        public static Publication Converter(PublicationViewModel publication) {
+            return new Publication() {
                 idPublication = publication.idPublication,
                 datetime = publication.datetime,
                 title = publication.title,
@@ -54,6 +62,14 @@ namespace FrontEnd.Models {
                 disLikes = publication.disLikes,
                 idUser = publication.idUser
             };
+        }
+
+        public static List<PublicationViewModel> Converter(List<Publication> publications) {
+            List<PublicationViewModel> publicationsVM = new List<PublicationViewModel>();
+            foreach (var p in publications) {
+                publicationsVM.Add((PublicationViewModel)Converter(p));
+            }
+            return publicationsVM;
         }
 
     }
