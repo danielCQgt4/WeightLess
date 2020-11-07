489ea3fb4f236704068cfa043fd0cc60d6ba796f
Santiago Hurtado <santiagohurtado99@hotmail.com>
2020-11-07 17:09:35 -0600

    Feat: Add alerts and show messages on log in

---
+++ FrontEnd/Public/js/controlAlerts.js
@@ -0,0 +1,15 @@
+﻿function showDiagSuccess(msg) {
+    Swal.fire({
+        icon: 'success',
+        title: 'Éxito',
+        text: msg
+    })
+}
+
+function showDiagError(msg) {
+    Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: msg
+    })
+}
